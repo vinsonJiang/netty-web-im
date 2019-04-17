5f9fad6747d7ea90699ae7c209e5f5ad93ea4cd5
vinsonJiang <1060312153@qq.com>
2019-04-17 21:44:07 +0800

    添加启动类

---
+++ server/src/main/java/io/vinson/im/server/ServerApplication.java
@@ -0,0 +1,16 @@
+package io.vinson.im.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: jiangweixin
+ * @date: 2019/4/17
+ */
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
